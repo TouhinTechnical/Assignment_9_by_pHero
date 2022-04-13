4bd5f436ef77ade59090109aefa5f597192ca222
TouhinTechnical <touhidulislam1090@gmail.com>
2022-04-13 12:13:55 +0600

    Create Error 404 page

--- product-review/src/component/Error404/Error404.js
+++ product-review/src/component/Error404/Error404.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Error404.css'
 
 const Error404 = () => {
     return (
         <div>
-            <h1>this page is avaiable</h1>
-            <p>404 page</p>
+            <div className="errorPage">
+                <img src="https://cdn.mos.cms.futurecdn.net/PuXipAW3AXUzUJ4uYyxPKC-1200-80.jpg" alt="404 page" />
+                <h4>Don't Be Sad😑😥 Please try again Right location</h4>
+                <Link to='/home' className='btn'>Back To Home </Link>
+            </div>
         </div>
     );
 };
